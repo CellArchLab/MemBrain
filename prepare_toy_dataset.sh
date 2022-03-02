d17e0b8092579255420c23d5f24676831257f954
Lamm <lorenz.lamm@tum.de>
2022-03-02 14:34:49 +0100

    Automate toy dataset

---
+++ prepare_toy_dataset.sh
@@ -0,0 +1,7 @@
+cd toy_data/tomograms/Tomo1/
+unzip membranes.zip
+wget https://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-10780/map/emd_10780.map.gz
+gzip -d emd_10780.map.gz
+mv emd_10780.map Tomo1_bin4.map
+rm emd_10780.map.gz
+
