dbce702326cf20cf63dc9c9cf1fd92caeda25b8e
LorenzLamm <34575029+LorenzLamm@users.noreply.github.com>
2022-03-04 10:28:42 +0100

    Update prepare_toy_dataset.sh

--- prepare_toy_dataset.sh
+++ prepare_toy_dataset.sh
@@ -3,5 +3,3 @@ unzip membranes.zip
 wget https://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-10780/map/emd_10780.map.gz
 gzip -d emd_10780.map.gz
 mv emd_10780.map Tomo1_bin4.map
-rm emd_10780.map.gz
-
